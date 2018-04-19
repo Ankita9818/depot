a1a54a7ca8beede6bbc9562b98eedb8196cd76eb
Ankita9818 <ankita.dixit@vinsol.com>
2018-04-19 19:25:32 +0530

    used reqex way for description length validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   PERMALINK_REGEX = %r{\A([a-z0-9]+(-[a-z0-9]+){2,})\Z}i
   DESCRIPTION_REGEX = %r{\A(\S+(\s+\S+){4,9})\s*\Z}i
-  HUMANIZED_ATTRIBUTES = {
-    split_description_to_words: "Description"
-  }
+  # HUMANIZED_ATTRIBUTES = {
+  #   split_description_to_words: "Description"
+  # }
 
   validates :title, :description, :price, :permalink, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01, allow_blank: true }
@@ -17,19 +17,19 @@ class Product < ApplicationRecord
   validates :image_url, presence: true, url: { allow_blank: true }
 
   # using regex
-  # validates :description, format: {
-  #   with: DESCRIPTION_REGEX,
-  #   allow_blank: true,
-  #   message: 'should have minimum 5 to 10 words'
-  # }
+  validates :description, format: {
+    with: DESCRIPTION_REGEX,
+    allow_blank: true,
+    message: 'should have minimum 5 to 10 words'
+  }
 
   #using length validator
-  validates :split_description_to_words, length: {
-    minimum: 5,
-    maximum: 10,
-    message: "must have between 5 to 10 words",
-    allow_blank: true
-  }
+  # validates :split_description_to_words, length: {
+  #   minimum: 5,
+  #   maximum: 10,
+  #   message: "must have between 5 to 10 words",
+  #   allow_blank: true
+  # }
 
   validates :discount_price, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
   validates_with DiscountValidator
@@ -54,11 +54,11 @@ class Product < ApplicationRecord
     end
   end
 
-  def split_description_to_words
-    description.split(' ')
-  end
+  # def split_description_to_words
+  #   description.split(' ')
+  # end
 
-  def self.human_attribute_name(attr, options = {})
-    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
-  end
+  # def self.human_attribute_name(attr, options = {})
+  #   HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  # end
 end
