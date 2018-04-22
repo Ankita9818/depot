8c833cc77f925a6a9c15345b1196c63a1a3b192d
Ankita9818 <ankita.dixit@vinsol.com>
2018-04-22 20:51:32 +0530

    removed commented code and correcte issue in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,6 @@
 class Product < ApplicationRecord
   PERMALINK_REGEX = %r{\A([a-z0-9]+(-[a-z0-9]+){2,})\Z}i
   DESCRIPTION_REGEX = %r{\A(\S+(\s+\S+){4,9})\s*\Z}i
-  # HUMANIZED_ATTRIBUTES = {
-  #   split_description_to_words: "Description"
-  # }
 
   validates :title, :description, :price, :permalink, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01, allow_blank: true }
@@ -22,25 +19,6 @@ class Product < ApplicationRecord
     allow_blank: true,
     message: 'should have minimum 5 to 10 words'
   }
-<<<<<<< HEAD
-
-  #using length validator
-  # validates :split_description_to_words, length: {
-  #   minimum: 5,
-  #   maximum: 10,
-  #   message: "must have between 5 to 10 words",
-  #   allow_blank: true
-  # }
-=======
->>>>>>> method names, constant etc changes committed
-
-  # #using length validator
-  # validates :split_description_to_words, length: {
-  #   minimum: 5,
-  #   maximum: 10,
-  #   message: "must have between 5 to 10 words",
-  #   allow_blank: true
-  # }
 
   validates :discount_price, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
   validates_with DiscountValidator
@@ -68,11 +46,11 @@ class Product < ApplicationRecord
     end
   end
 
-  # def set_default_title
-  #   self.title = 'abc' if title.blank?
-  # end
+  def set_default_title
+    self.title = 'abc'
+  end
 
-  # def set_default_discount_price
-  #   self.discount_price = price unless discount_price?
-  # end
+  def set_default_discount_price
+    self.discount_price = price
+  end
 end
