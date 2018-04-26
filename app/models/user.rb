c32bec0a6c3dada2eab2b3c1994bb67407e9150e
Ankita9818 <ankita.dixit@vinsol.com>
2018-04-26 17:20:25 +0530

    used conditional validation for line_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   has_secure_password
   after_destroy :ensure_a_user_remains
-  after_create_commit :send_welcome_email
+  # after_create_commit :send_welcome_email
   before_update { ensure_not_depot_admin('update') } if :depot_admin?
   before_destroy { ensure_not_depot_admin('delete') } if :depot_admin?
 
