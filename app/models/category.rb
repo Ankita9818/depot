35e003fb32bf9e7ab03622ed63a5b470f03e91ef
Ankita9818 <ankita.dixit@vinsol.com>
2018-05-03 19:42:39 +0530

    recomputed product count on parent_category change

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,6 +14,8 @@ class Category < ApplicationRecord
   has_many :products, dependent: :restrict_with_error
   has_many :sub_products, through: :sub_categories, source: :products, dependent: :restrict_with_error
 
+  after_update_commit :ensure_single_level_nesting, :evaluate_products_count
+
   def recompute_products_count
     self.products_count = count_products
     save
@@ -22,13 +24,18 @@ class Category < ApplicationRecord
 
   private
 
+  def evaluate_products_count
+    Category.find(parent_category_id_previous_change.first).recompute_products_count if previous_changes.key?(:parent_category_id) && parent_category_id_previous_change.first.present?
+    parent_category.recompute_products_count if parent_category.present?
+  end
+
   def ensure_single_level_nesting
-    if parent_category_id? && parent_category.parent_category.present?
+    if parent_category_id? && parent_category.parent_category.present? || sub_categories.present? && parent_category.present?
       errors.add(:base, 'parent_category is itself a sub category')
     end
   end
 
   def count_products
-    Product.where(category_id: id).count + sub_products.count
+    products.count + sub_products.count
   end
 end
